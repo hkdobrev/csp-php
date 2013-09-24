f7983458eb4febbafd69c2d075a635a1c1556233
Haralan Dobrev <hkdobrev@gmail.com>
2013-09-24 10:44:52 +0300

    URI for ReportURIDirective

--- lib/CSP/URI.php
+++ lib/CSP/URI.php
@@ -1,3 +1,22 @@
 <?php
 
-class URI {}
+class URI implements Renderable {
+
+	protected $_uri;
+
+	public function __construct($uri)
+	{
+		$this->_uri = $uri;
+		$this->check();
+	}
+
+	public function check()
+	{
+		return TRUE;
+	}
+
+	public function render()
+	{
+		return $this->_uri;
+	}
+}
