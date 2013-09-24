3c1965c631312a7f6e4136235f400083f2375b63
Haralan Dobrev <hkdobrev@gmail.com>
2013-09-24 10:45:52 +0300

    Token and TokenList for SandboxDirective

--- lib/CSP/Sandbox/Token.php
+++ lib/CSP/Sandbox/Token.php
@@ -2,4 +2,24 @@
 
 namespace CSP\Token;
 
-class Token {}
+class Token implements Renderable {
+
+	protected $_value;
+
+	public function getValue()
+	{
+		return $this->getValue();
+	}
+
+	public function setValue($value)
+	{
+		$this->_value = $value;
+
+		return $this;
+	}
+
+	public function render()
+	{
+		return $this->getValue();
+	}
+}
---
+++ lib/CSP/Sandbox/TokenList.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace CSP\Sandbox;
+
+use CSP\Util\RenderableList;
+
+class TokenList extends RenderableList {}
