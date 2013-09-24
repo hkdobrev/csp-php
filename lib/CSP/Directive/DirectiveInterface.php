489872a334cc0d7fbf3447fefba58666be0729d3
Haralan Dobrev <hkdobrev@gmail.com>
2013-09-24 10:43:52 +0300

    DirectiveInterface should be used for all directives

---
+++ lib/CSP/Directive/DirectiveInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CSP\Directive;
+
+use CSP\Renderable;
+
+interface DirectiveInterface extends Renderable {
+
+	public function getName();
+
+	public function getValue();
+
+	public function setValue($value);
+}
