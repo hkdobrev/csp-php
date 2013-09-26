b0a2bd5644ec2d415a9e180fcc3d1dffdbc2b7d9
Haralan Dobrev <hkdobrev@gmail.com>
2013-09-26 10:53:11 +0300

    Unit tests for DirectiveList

---
+++ tests/CSP/Test/Directive/DirectiveListTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CSP\Test\Directive;
+
+use CSP\Test\TestCase;
+use CSP\Directive\DirectiveList;
+use CSP\Test\Util\Render;
+
+class DirectiveListTest extends TestCase {
+
+	public function testRenderableList()
+	{
+		$this->assertInstanceOf('CSP\Util\RenderableList', new DirectiveList);
+	}
+
+	public function testRenderWithSemiColonDelimiter()
+	{
+		$directiveList = new DirectiveList(array(
+			new Render('A'),
+			new Render('B'),
+			new Render('C'),
+		));
+
+		$this->assertSame('A; B; C', $directiveList->render());
+	}
+}
